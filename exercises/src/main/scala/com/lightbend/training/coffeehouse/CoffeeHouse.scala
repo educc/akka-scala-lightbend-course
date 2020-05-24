61759b3fa349b2dc72e9591773287ee103a5cddb
ecacho <ecachocasas@gmail.com>
2020-05-24 17:56:23 -0500

    scheduling

--- exercises/src/main/scala/com/lightbend/training/coffeehouse/CoffeeHouse.scala
+++ exercises/src/main/scala/com/lightbend/training/coffeehouse/CoffeeHouse.scala
@@ -1,7 +1,11 @@
 package com.lightbend.training.coffeehouse
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import com.lightbend.training.coffeehouse.CoffeeHouse.CreateGuest
+
+import scala.concurrent.duration._
 
 object CoffeeHouse {
   case class CreateGuest(favoriteCoffee: Coffee)
@@ -13,10 +17,12 @@ class CoffeeHouse extends Actor with ActorLogging {
 
   log.debug("CoffeeHouse Open")
 
+  private val finishCoffeeDuration: FiniteDuration =
+    context.system.settings.config.getDuration("coffee-house.guest.finish-coffee-duration", TimeUnit.MILLISECONDS).millis
   private val waiter: ActorRef = createWaiter()
 
   protected def createGuest(coffee: Coffee): ActorRef = {
-    context.actorOf(Guest.props(waiter, coffee))
+    context.actorOf(Guest.props(waiter, coffee, finishCoffeeDuration  ))
   }
   protected def createWaiter(): ActorRef = context.actorOf(Waiter.props(), "waiter")
 
--- exercises/src/main/scala/com/lightbend/training/coffeehouse/Guest.scala
+++ exercises/src/main/scala/com/lightbend/training/coffeehouse/Guest.scala
@@ -1,24 +1,34 @@
 package com.lightbend.training.coffeehouse
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, Timers}
 import com.lightbend.training.coffeehouse.Guest.CoffeeFinished
+
+import scala.concurrent.duration.FiniteDuration
 
 object Guest {
   case object CoffeeFinished
 
-  def props(waiter: ActorRef, favoriteCoffee: Coffee): Props = {
-    Props(new Guest(waiter, favoriteCoffee))
+  def props(waiter: ActorRef, favoriteCoffee: Coffee, finishCoffeeDuration: FiniteDuration): Props = {
+    Props(new Guest(waiter, favoriteCoffee, finishCoffeeDuration))
   }
 }
 
-class Guest(waiter: ActorRef, favoriteCoffee: Coffee) extends Actor with ActorLogging {
+class Guest(waiter: ActorRef, favoriteCoffee: Coffee, finishCoffeeDuration: FiniteDuration)
+  extends Actor with ActorLogging with Timers {
   private var coffeeCount: Int = 0
+
+  orderCoffee()
 
   override def receive: Receive = {
     case Waiter.CoffeeServed(coffee) =>
       coffeeCount += 1
       log.info(s"Enjoying my $coffeeCount yummy $coffee")
+      timers.startSingleTimer("coffee-finished", CoffeeFinished, finishCoffeeDuration)
     case CoffeeFinished =>
-      waiter ! Waiter.ServeCoffee(favoriteCoffee)
+      orderCoffee()
+  }
+
+  private def orderCoffee() = {
+    waiter ! Waiter.ServeCoffee(favoriteCoffee)
   }
 }
